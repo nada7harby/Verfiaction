d34f089c357f3978a974b82d0e520df7e17ca555
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-05-12 15:25:03 +0300

    all

--- script.js
+++
@@ -1,9 +0,0 @@
-function saveDraft() {
-    alert("Verification saved in drafts!");
-}
-
-document.getElementById('verificationForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert("Verification submitted!");
-    // يمكنك هنا إضافة كود إرسال البيانات للسيرفر
-}); 
